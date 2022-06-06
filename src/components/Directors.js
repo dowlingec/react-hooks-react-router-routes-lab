5131d9b336f9c4b5030b9e314618e62660bdf425
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-05 20:03:03 -0400

    commit tree e8c2626d7360573c97fdd9c343aa9b6b7aa2b9c0

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    <h1>Directors Page</h1>
+    { directors.map((dir) => {
+      return(
+        <div>
+          <h3>Name: {dir.name}</h3>
+          <p>Movies:</p>
+          <ul>{dir.movies.map((el) => {
+            return <li>{el}</li>
+            })}</ul>
+        </div>
+      )
+    })}
+  </div>);
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+      <h1>Movies Page</h1>
+        {movies.map((movie) => {
+          return (
+            <div>
+              <h3>Name: {movie.title}</h3>
+              Time: {movie.time}
+              <ul>Movies:
+                {movie.genres.map((el) => {return (<li>{el}</li>)})}
+              </ul>
+            </div>
+          )
+        })}
+  </div>);
 }
 
 export default Movies;
