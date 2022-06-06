bd62aead6fd74d629adc61b802ffc585743b1f87
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-05 20:09:21 -0400

    Done I think

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    <h1>Actors Page</h1>
+    { actors.map((act) => {
+      return(
+        <div>
+          <h3>Name: {act.name}</h3>
+          <ul>Movies: {act.movies.map((el) => {
+            return <li>{el}</li>
+            })}</ul>
+        </div>
+      )
+    })}
+  </div>);
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <li><NavLink to="/" exact >Home</NavLink></li>
+    <li><NavLink to="/movies" exact >Movies</NavLink></li>
+    <li><NavLink to="/directors" exact >Directors</NavLink></li>
+    <li><NavLink to="/actors">Actors</NavLink></li>
+  </div>;
 }
 
 export default NavBar;
